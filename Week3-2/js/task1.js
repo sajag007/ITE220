0d2b5c2032f8736202c8d3d8147090f4b09c9c95
Sajag <sajag.chaturvedi@students.stamford.edu>
2016-05-26 17:23:44 +0530

    Updated Work

--- Week3-2/js/task1.js
+++ Week3-2/js/task1.js
@@ -1,29 +1,28 @@
+(function(){
 var task1 = {
 	render: function(){
 		document.getElementById("screenX").textContent = window.screenX;
 		document.getElementById("screenY").textContent = window.screenY;
-
 		document.getElementById("screenWidth").textContent = window.innerWidth;
 		document.getElementById("screenHeight").textContent = window.innerHeight;
-		document.getElementById("location").textContent = window.location;
-		"Sajag Chaturvedi",
-		"Student",
-		"sajag007.github.io/ITE220"
-		
+		document.getElementById("location").textContent = window.location.pathname;
 	},
 	resize: function(){
-		window.addEventListner("resize", this.render);
-	},
-	devInfo: function() {
+		window.addEventListener("resize", this.render);
+	}, 
+	
+	devInfo: function(){
 		var btnDev = document.getElementById("dev-info");
-		btnDev.addEventListner("click", function() {
-
-
-		});
-
-
-	}
+		btnDev.addEventListener("click", function(){
+			var nameText = ["Sajag Chaturvedi", "IT Student"]; 
+			var nameLink = "<a href='https://github.com/sajag007/ITE220' target='_blank'>https://github.com/sajag007</a>";
+			var txt = "" + nameText[0] + "" + "<br>" + nameText[1] + "" + "<br>" + nameLink;
+			var nameElement = document.getElementById("dev-info-container").innerHTML = txt;
+			});
+		}
+		
 }
 task1.render();
 task1.resize();
 task1.devInfo();
+})();
